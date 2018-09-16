bdd679b8cd000ca562096350d04ee727e1171ec6
Myada Roshdi <30698281+MyadaRoshdi@users.noreply.github.com>
2018-09-16 09:22:21 -0400

    reverting as project repo

--- src/tools.h
+++ src/tools.h
@@ -2,20 +2,13 @@
 #define TOOLS_H_
 #include <vector>
 #include "Eigen/Dense"
-#include <iostream>
-#include <fstream>
 
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 using namespace std;
 
 class Tools {
-private:
-	ofstream  L_out, R_out;
 public:
-
-	
-
   /**
   * Constructor.
   */
@@ -30,8 +23,6 @@ public:
   * A helper method to calculate RMSE.
   */
   VectorXd CalculateRMSE(const vector<VectorXd> &estimations, const vector<VectorXd> &ground_truth);
-  void R_write_line(double NIS_R);
-  void L_write_line(double NIS_L);
 
 };
 
