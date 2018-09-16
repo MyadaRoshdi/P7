6c5bf6b49c03fc312677e3ff8ccd7966c5b4fda6
Myada Roshdi <30698281+MyadaRoshdi@users.noreply.github.com>
2018-09-16 09:23:04 -0400

    Update tools.cpp

--- src/tools.cpp
+++ src/tools.cpp
@@ -5,83 +5,42 @@ using Eigen::VectorXd;
 using Eigen::MatrixXd;
 using std::vector;
 
-Tools::Tools() {
+Tools::Tools() {}
 
-	
-   L_out.open("L_out.txt", ios::out | ios::ate | ios::app);
-   R_out.open("R_out.txt", ios::out | ios::ate | ios::app);
-
-
-}
-
-Tools::~Tools() {
-	
-	L_out.close();
-	R_out.close();
-}
+Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
   /**
-	    * Calculate the RMSE here.
-	 */
+  TODO:
+    * Calculate the RMSE here.
+  */
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
 
-	VectorXd rmse(4);
+  if(estimations.size() == 0){
+    cout << "ERROR - CalculateRMSE () - The estimations vector is empty" << endl;
+    return rmse;
+  }
 
-	rmse << 0,0,0,0;
+  if(ground_truth.size() == 0){
+    cout << "ERROR - CalculateRMSE () - The ground-truth vector is empty" << endl;
+    return rmse;
+  }
 
-	// check the validity of the following inputs:
-	//  * the estimation vector size should not be zero
-	//  * the estimation vector size should equal ground truth vector size
-	if(estimations.size() != ground_truth.size() || estimations.size() == 0){
+  unsigned int n = estimations.size();
+  if(n != ground_truth.size()){
+    cout << "ERROR - CalculateRMSE () - The ground-truth and estimations vectors must have the same size." << endl;
+    return rmse;
+  }
 
-		cout << "Invalid estimation or ground_truth data" << endl;
-		return rmse;
-	}
+  for(unsigned int i=0; i < estimations.size(); ++i){
+    VectorXd diff = estimations[i] - ground_truth[i];
+    diff = diff.array()*diff.array();
+    rmse += diff;
+  }
 
-	//accumulate squared residuals
-	for(unsigned int i=0; i < estimations.size(); ++i){
-
-		VectorXd residual = estimations[i] - ground_truth[i];
-
-		//coefficient-wise multiplication
-		residual = residual.array()*residual.array();
-		rmse += residual;
-	}
-
-	//calculate the mean
-	rmse = rmse/estimations.size();
-
-	//calculate the squared root
-	rmse = rmse.array().sqrt();
-
-	//return the result
-	return rmse;
-
+  rmse = rmse / n;
+  rmse = rmse.array().sqrt();
+  return rmse;
 }
-
-void Tools::L_write_line(double NIS_L)
-{
-	if (L_out.is_open()){
-
-		L_out << NIS_L << ",";
-	}
-	else{
-
-		cout << "Cannot open L_out file!!!" << endl;
-	}
-	
-}
-
-void Tools::R_write_line(double NIS_R)
-{
-	if (R_out.is_open()) {
-
-		R_out << NIS_R << ",";
-	}
-	else{
-
-		cout << "Cannot open R_out file!!!" << endl;
-	}
-}
-
